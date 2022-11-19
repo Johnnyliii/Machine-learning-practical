945f8c8a269f1c1b39d54a61b4f4616b7b3c6987
Johnnyliii <117656994+Johnnyliii@users.noreply.github.com>
2022-11-19 11:38:33 +0000

    Update experiment_builder.py

--- pytorch_mlp_framework/experiment_builder.py
+++ pytorch_mlp_framework/experiment_builder.py
@@ -172,8 +172,8 @@ class ExperimentBuilder(nn.Module):
     def run_train_iter(self, x, y):
         
         self.train()  # sets model to training mode (in case batch normalization or other methods have different procedures for training and evaluation)
-        x, y = x.float().to(torch.device('cuda')), y.long().to(torch.device('cuda'))  # send data to device as torch tensors
-        out = self.model.forward(x).to(torch.device('cuda'))  # forward the data in the model
+        x, y = x.float().to(device=self.device), y.long().to(device=self.device)  # send data to device as torch tensors
+        out = self.model.forward(x)  # forward the data in the model
 
 
         loss = F.cross_entropy(input=out, target=y)  # compute loss
